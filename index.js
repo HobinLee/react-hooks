8f772a39402e746f3e372660cfe7c50dba661776
atthemomentkor <at.the.moment.kor@gmail.com>
2021-09-06 12:43:54 +0900

    fix: dependency에 typescript 제거 및 index.js 위치 변경

---
+++ index.js
@@ -0,0 +1,4 @@
+export { default as useDebounce } from "./src/useDebounce";
+export { default as useLazyLoad } from "./src/useLazyLoad";
+export { default as useThrottle } from "./src/useThrottle";
+export { default as useValidation } from "./src/useValidation";
